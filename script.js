ad1b065e40f0d7277da051d2cf2d6e2e96d72502
Hely Mendez <menzmo77@gmail.com>
2021-10-26 21:44:18 -0600

    desplega el camino y los kilometros a recorrer

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-
-const encontrarCostoMenor = (pesos, procesado) => {
+// devuelve el nodo con el peso menor
+const encontrarCostoMenor = (pesos, visitado) => {
   return Object.keys(pesos).reduce((pesoMenor, nodo) => {
-      if (pesoMenor === null || pesos[nodo] < pesos[pesoMenor]){ 
-        if (!procesado.includes(nodo)) {
+    if (pesoMenor === null || pesos[nodo] < pesos[pesoMenor]) {
+      if (!visitado.includes(nodo)) {
         pesoMenor = nodo;
       }
     }
-      return pesoMenor;
+    return pesoMenor;
   }, null);
 };
 
 // función que devuelve el costo minimo y el camino final
 const dijkstra = (grafo, nodoInicial, nodoDestino) => {
+
+  // asigna el nodo destino con la etiqueta "infinity"
   let pesos = {};
   pesos[nodoDestino] = 'Infinity'
+
+  //Hace una copia desde el nodo origen
   pesos = Object.assign(pesos, grafo[nodoInicial]);
   console.log('Inicial `pesos`: ')
   console.log(pesos)
 
   // recorrido de caminos
-  const nodosPadre = {nodoDestino: null};
+  const nodosPadre = {
+    nodoDestino: null
+  };
+
   for (let hijo in grafo[nodoInicial]) {
     nodosPadre[hijo] = nodoInicial;
   }
@@ -30,44 +37,54 @@ const dijkstra = (grafo, nodoInicial, nodoDestino) => {
   const nodosVisitados = [];
 
   // Asigna el nodo inical. Toma el peso menor
-
   let nodo = encontrarCostoMenor(pesos, nodosVisitados);
-  console.log('Initial `node`: ', nodo)
+  console.log('Nodo `inicial`: ', nodo)
 
   while (nodo) {
     console.log(`***** 'Nodo Actual': ${nodo} *****`)
     let peso = pesos[nodo];
     let hijosDelNodo = grafo[nodo];
-  
+
     for (let n in hijosDelNodo) {
-       if(String(n) === String(nodoInicial)){
+
+      if (String(n) === String(nodoInicial)) {
         console.log('Buscando la ruta mas corta');
-       }else{
-        console.log('Nodo inicial '+ nodoInicial);
-        console.log('Evaluando el peso del nodo '+n +'... Buscando desde el nodo '+nodo);
-        console.log('ultimo peso registrado '+pesos[n]);
+      } else {
+        console.log('Nodo inicial ' + nodoInicial);
+        console.log('Evaluando el peso del nodo ' + n + '... Buscando desde el nodo ' + nodo);
+        console.log('ultimo peso registrado ' + pesos[n]);
+
+        // guarda el peso nuevo en una variable
         let nuevoPeso = peso + hijosDelNodo[n];
-        console.log('Nuevo peso: '+ nuevoPeso);
-        if(!pesos[n] || pesos[n] > nuevoPeso){
-          pesos[n]  = nuevoPeso;
+        console.log('Nuevo peso: ' + nuevoPeso);
+
+        // verifica el nuevo peso, si es menor al peso antes registrado se toma ese peso comoel mejor
+        if (!pesos[n] || pesos[n] > nuevoPeso) {
+          pesos[n] = nuevoPeso;
           nodosPadre[n] = nodo;
           console.log('Actualizado el peso y los nodos padre')
-        }else{
+        } else {
           console.log('Un camino mas corto ya existe')
         }
-       }
+      }
     }
+    // Se marcan los nodos visitados
     nodosVisitados.push(nodo);
+    // continua buscando el camino menor, hasta que todos los nodos sean visitados
     nodo = encontrarCostoMenor(pesos, nodosVisitados);
   }
-  console.log('while loop ends: ')
+  console.log('Termina el ciclo')
 
   let caminoMasCorto = [nodoDestino];
   let padre = nodosPadre[nodoDestino];
+  console.log("camino " + caminoMasCorto)
+
+  //asigna al caminoMasCorto los nodos padre
   while (padre) {
     caminoMasCorto.push(padre);
     padre = nodosPadre[padre];
   }
+  
   caminoMasCorto.reverse();
 
   const resultado = {
@@ -77,20 +94,33 @@ const dijkstra = (grafo, nodoInicial, nodoDestino) => {
 
 
 
-// Crea el elemento lista en el DOM
-  div =document.getElementById('resultado');
-  var ul = document.createElement('ul'), li;
-    console.log(resultado)
-  for(let i=0; i<resultado.camino.length; i++){
-        console.log(resultado.camino[i])
-      li= document.createElement('li');
-      li.appendChild(document.createTextNode(resultado.camino[i]));
-      ul.appendChild(li);
+  // Crea el elemento lista en el DOM
+  div = document.getElementById('resultado');
+  divRow2 = document.createElement('div')
+  divRow2.classList.add('row', 'shadow-sm', 'p-3', 'mb-5', 'bg-white', 'rounded');
+  
+  div.appendChild(divRow2);
+  divCol1 = document.createElement('div');
+  divCol1.classList.add('col-md-6');
+  divRow2.appendChild(divCol1);
+
+  var divCol2 = document.createElement('div');
+  divCol2.classList.add('col-md-6', 'display-6', 'font-weight-bold');
+  var content = document.createTextNode(resultado.distancia +' KM')
+  divCol2.appendChild(content)
+  divRow2.appendChild(divCol2);
+
+  var ul = document.createElement('ul'),
+    li;
+  for (let i = 0; i < resultado.camino.length; i++) {
+    console.log(resultado.camino[i])
+    li = document.createElement('li');
+    li.appendChild(document.createTextNode(resultado.camino[i]));
+    ul.appendChild(li);
   }
 
-      div.appendChild(ul);
-      document.body.appendChild(div)
+  divCol1.appendChild(ul);
+  document.body.appendChild(div)
   return resultado;
 
 };
-
--- script2.js
+++ script2.js
@@ -1,41 +1,46 @@
+// arreglo para el select
+departamentos = [
+	// valor        clave
+	["GUATEMALA", "guatemala"],
+	["El PROGRESO", "el_progreso"],
+	["SACATEPEQUEZ", "sacatepequez"],
+	["CHIMALTENANGO", "chimaltenango"],
+	["ESCUINTLA", "escuintla"],
+	["SANTA ROSA", 'santa_rosa'],
+	["SOLOLA", "solola"],
+	["TOTONICAPAN", "totonicapan"],
+	["QUEZALTENANGO", "quezaltenango"],
+	["SUCHITEPEQUEZ", "suchitepequez"],
+	["RETALHULEU", "retalhuleu"],
+	["SAN MARCOS", "san_marcos"],
+	["HUEHUETENANGO", "huehuetenango"],
+	["QUICHE", "quiche"],
+	["BAJA VERAPAZ", "baja_verapaz"],
+	["ALTA VERAPAZ", "alta_verapaz"],
+	["PETEN", "peten"],
+	["IZABAL", "izabal"],
+	["ZACAPA", "zacapa"],
+	["CHIQUIMULA", "chiquimula"],
+	["JALAPA", "jalapa"],
+	["JUTIAPA", "jutiapa"]
+];
+// ORIGEN 
 
-	departamentos = [
-			["GUATEMALA","guatemala"],
-			["El PROGRESO", "el_progreso"],
-			["SACATEPEQUEZ", "sacatepequez"],
-			["CHIMALTENANGO", "chimaltenango"],
-			["ESCUINTLA", "escuintla"],
-			["SANTA ROSA", 'santa_rosa'],
-			["SOLOLA","solola"],
-			["TOTONICAPAN", "totonicapan"],
-			["QUEZALTENANGO", "quezaltenango"],
-			["SUCHITEPEQUEZ", "suchitepequez"],
-			["RETALHULEU", "retalhuleu"],
-			["SAN MARCOS", "san_marcos"],
-			["HUEHUETENANGO", "huehuetenango"],
-			["QUICHE", "quiche"],
-			["BAJA VERAPAZ", "baja_verapaz"],
-			["ALTA VERAPAZ", "alta_verapaz"],
-			["PETEN", "peten"],
-			["IZABAL", "izabal"],
-			["ZACAPA", "zacapa"],
-			["CHIQUIMULA", "chiquimula"],
-			["JALAPA", "jalapa"],
-			["JUTIAPA","jutiapa"]
-			];
+// Crea el elemendo div y le agrega la clase row
 var divRow = document.createElement('div')
 divRow.classList.add('row');
 
 // crea el elemento select de origen
 var form = document.getElementById('origen');
 
+// Crea un elemento Select
 var selectList = document.createElement('select');
 selectList.id = 'select-origen';
 selectList.classList.add('form-control');
-
 form.appendChild(selectList);
 
-for(var i = 0; i<departamentos.length; i++){
+// por cada elemento dentro del arreglo de departamentos crea una opcion
+for (var i = 0; i < departamentos.length; i++) {
 	var option = document.createElement("option");
 	option.value = departamentos[i][1];
 	option.id = departamentos[i][0];
@@ -45,8 +50,10 @@ for(var i = 0; i<departamentos.length; i++){
 
 	document.body.appendChild(form);
 }
+// insert el select en el DOM
 divRow.appendChild(form);
 
+// DESTINO
 // crea el elemento select de Destino
 var form2 = document.getElementById('destino');
 
@@ -56,7 +63,7 @@ selectList2.classList.add('form-control');
 
 form2.appendChild(selectList2);
 
-for(var i = 0; i<departamentos.length; i++){
+for (var i = 0; i < departamentos.length; i++) {
 	var option = document.createElement("option");
 	option.value = departamentos[i][1];
 	option.text = departamentos[i][0];
@@ -68,7 +75,8 @@ for(var i = 0; i<departamentos.length; i++){
 divRow.appendChild(form2);
 document.body.appendChild(divRow)
 
+// crea un nuevo contenedor para alojar los resultados
 let contenedor = document.createElement('div');
-contenedor.id='resultado'
+contenedor.id = 'resultado'
 contenedor.classList.add('card')
 document.body.appendChild(contenedor);
